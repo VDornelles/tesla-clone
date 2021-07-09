321f6d6916e7c6d799a3c2c6210f42d258dc5a4c
Vinicius Dornelles de Sousa <vinicius_sousa@hst.com.br>
2021-07-09 19:29:51 -0300

    Adding text and button animation

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ButtonGroup, Buttons, DownArrow, ItemText, LeftButton, RightButton, Wrap } from './styles'
-
+import Fade from 'react-reveal/Fade'
 
 function Section({
   title, 
@@ -11,22 +11,27 @@ function Section({
 }) {
   return (
     <Wrap bgImage={backgroundImg}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p> {description} </p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p> {description} </p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup >
-          <LeftButton>
-            {leftBtnText}
-          </LeftButton>
-          {rightBtnText && 
-            <RightButton>
-              {rightBtnText}
-            </RightButton>
-          }
-          
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup >
+            <LeftButton>
+              {leftBtnText}
+            </LeftButton>
+            {
+              rightBtnText && 
+                <RightButton>
+                  {rightBtnText}
+                </RightButton>
+            }
+            
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg"/>
       </Buttons>
     </Wrap>
